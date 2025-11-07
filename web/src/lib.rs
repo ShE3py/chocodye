9cdcd5926847001e9a5e0776db10ea873c47d2bd
Lieselotte <52315535+she3py@users.noreply.github.com>
2025-11-08 00:09:51 +0100

    Flatten `request_menu` with `let else`s

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -13,10 +13,14 @@ pub static LANGS: [&str; 4] = [LANG_EN, LANG_FR, LANG_DE, LANG_JP];
 #[no_mangle]
 pub static LANG_SIZES: [usize; 4] = [LANG_EN.len(), LANG_FR.len(), LANG_DE.len(), LANG_JP.len()];
 
+fn cvt(dye: i32) -> Option<Dye> {
+    Dye::VALUES.get(usize::try_from(dye).ok()?).copied()
+}
+
 #[no_mangle]
 pub extern "C" fn make_meal(starting_dye: i32, final_dye: i32) -> Option<SnackList> {
-    let starting_dye = Dye::VALUES.get(usize::try_from(starting_dye).ok()?).copied()?;
-    let final_dye = Dye::VALUES.get(usize::try_from(final_dye).ok()?).copied()?;
+    let starting_dye = cvt(starting_dye)?;
+    let final_dye = cvt(final_dye)?;
 
     let meal = chocodye::make_meal(starting_dye, final_dye);
     Some(SnackList::from(meal.as_slice()))
@@ -24,54 +28,51 @@ pub extern "C" fn make_meal(starting_dye: i32, final_dye: i32) -> Option<SnackLi
 
 #[no_mangle]
 pub extern "C" fn request_menu(starting_dye: i32, final_dye: i32, sl: Option<SnackList>, lang: i32) {
-    if let Some(starting_dye) = usize::try_from(starting_dye).ok().and_then(|n| Dye::VALUES.get(n)).copied() {
-        if let Some(snacks) = sl {
-            if let Some(lang) = usize::try_from(lang).ok().and_then(|n| Lang::VALUES.get(n)).copied() {
-                let bundle = lang.into_bundle();
+    let Some(((starting_dye, final_dye), snacks)) = cvt(starting_dye).zip(cvt(final_dye)).zip(sl) else {
+        return;
+    };
+    let Some(bundle) = usize::try_from(lang).ok().and_then(|n| Lang::VALUES.get(n)).copied().map(Lang::into_bundle) else {
+        return;
+    };
 
-                let menu = chocodye::make_menu(starting_dye, snacks);
+    let menu = chocodye::make_menu(starting_dye, snacks);
 
-                let mut written = String::new();
+    let mut written = String::new();
 
-                write!(written, "<p>{}</p><ul>", message!(&bundle, "required-fruits")).unwrap();
-                for (snack, count) in snacks.into_iter().filter(|(_, count)| *count > 0) {
-                    write!(written, "<li>{}</li>", snack.quantified_name(&bundle, count as u32)).unwrap();
-                }
+    _ = write!(written, "<p>{}</p><ul>", message!(&bundle, "required-fruits"));
+    for (snack, count) in snacks.into_iter().filter(|(_, count)| *count > 0) {
+        _ = write!(written, "<li>{}</li>", snack.quantified_name(&bundle, count as u32));
+    }
+    if snacks.is_empty() {
+        _ = write!(written, r#"<li><span class="emph">{}</li></ul>"#, message!(&bundle, "none"));
+    }
 
-                if snacks.is_empty() {
-                    write!(written, r#"<li><span class="emph">{}</li></ul>"#, message!(&bundle, "none")).unwrap();
-                }
-                else {
-                    write!(written, "</ul>").unwrap();
+    else {
+        _ = write!(written, "</ul>");
 
-                    write!(written, "<p>{}</p><ul>", message!(&bundle, "feed-order")).unwrap();
-                    for (snack, count) in menu {
-                        write!(written, "<li>{}</li>", snack.quantified_name(&bundle, count as u32)).unwrap();
-                    }
-                    write!(written, "</ul>").unwrap();
+        _ = write!(written, "<p>{}</p><ul>", message!(&bundle, "feed-order"));
+        for (snack, count) in menu {
+            _ = write!(written, "<li>{}</li>", snack.quantified_name(&bundle, count as u32));
+        }
+        _ = write!(written, "</ul>");
 
-                    if let Some(final_dye) = usize::try_from(final_dye).ok().and_then(|n| Dye::VALUES.get(n)).copied() {
-                        let ds = starting_dye.distance(final_dye);
-                        let dd = Dye::DEFAULT_CHOCOBO_COLOR.distance(final_dye);
+        let ds = starting_dye.distance(final_dye);
+        let dd = Dye::DEFAULT_CHOCOBO_COLOR.distance(final_dye);
 
-                        #[expect(clippy::cast_precision_loss, reason = "precision not required as we're rounding to .1")]
-                        if ds > dd {
-                            let ss = snacks.sum();
-                            let ds = chocodye::make_meal(Dye::DEFAULT_CHOCOBO_COLOR, final_dye).len();
+        #[expect(clippy::cast_precision_loss, reason = "precision not required as we're rounding to .1")]
+        if ds > dd {
+            let ss = snacks.sum();
+            let ds = chocodye::make_meal(Dye::DEFAULT_CHOCOBO_COLOR, final_dye).len();
 
-                            write!(written, "<p>{}</p>", message!(&bundle, "han-lemon-note", { "ratio" = format!("{:.1}", 100_f32 * (1_f32 - (ds as f32 / ss as f32))) })).unwrap();
-                        }
-                    }
-                }
-
-                #[link(wasm_import_module = "chocoweb")]
-                extern "C" {
-                    fn update_menu(dom_ptr: *const u8, dom_len: usize);
-                }
-
-                // SAFETY: trivial
-                unsafe { update_menu(written.as_ptr(), written.len()) };
-            }
+            _ = write!(written, "<p>{}</p>", message!(&bundle, "han-lemon-note", { "ratio" = format!("{:.1}", 100_f32 * (1_f32 - (ds as f32 / ss as f32))) }));
         }
     }
+
+    #[link(wasm_import_module = "chocoweb")]
+    unsafe extern "C" {
+        fn update_menu(dom_ptr: *const u8, dom_len: usize);
+    }
+
+    // SAFETY: trivial
+    unsafe { update_menu(written.as_ptr(), written.len()) };
 }
